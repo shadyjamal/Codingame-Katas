965eb5d15a9a83f296aa8fa15a0e9919de91e2c7
shadyjamal <chadijamal@gmail.com>
2020-06-23 22:49:38 +0100

    Coding practice exercices

--- Practice/computational complexity/Complexity-LinearReg.js
+++ Practice/computational complexity/Complexity-LinearReg.js
@@ -39,35 +39,33 @@ for (let i = 0; i < N; i++) {
   varX.push(num);
   varY.push(t);
 }
-while (true) {
-  let resTab = [];
+let resTab = [];
 
-  // Calulates PCC for (X, Y) && (X^2, Y) && (X^3, Y)
-  for (let i = 1; i <= 3; i++) {
-    const tabX = varX.map((val) => Math.pow(val, i));
-    let res = PearsonCoeff(tabX, varY);
-    let nPow = "n";
-    if (res === "O(1)") return console.log(res);
-    else {
-      if (i > 1) nPow += `^${i}`;
-      resTab.push([res, `O(${nPow})`]);
-    }
-  }
-
-  // Calulates PCC for (Log(X), Y) && (X * Log(X), Y) && (X^2 * Log(X), Y) && (X^3 * Log(X), Y)
-  for (let i = 0; i <= 3; i++) {
-    const tabX = varX.map((val) => Math.pow(val, i) * Math.log(val));
-    let nPow = "n";
+// Calulates PCC for (X, Y) && (X^2, Y) && (X^3, Y)
+for (let i = 1; i <= 3; i++) {
+  const tabX = varX.map((val) => Math.pow(val, i));
+  let res = PearsonCoeff(tabX, varY);
+  let nPow = "n";
+  if (res === "O(1)") return console.log(res);
+  else {
     if (i > 1) nPow += `^${i}`;
-    resTab.push([PearsonCoeff(tabX, varY), `O(${i ? `${nPow} ` : ""}log n)`]);
+    resTab.push([res, `O(${nPow})`]);
   }
-
-  // Calulates PCC for (2^X, Y)
-  const tabX = varX.map((val) => Math.pow(2, val));
-  resTab.push([PearsonCoeff(tabX, varY), "O(2^n)"]);
-
-  // sort to find the best Correlation
-  resTab.sort((a, b) => b[0] - a[0]);
-  
-  return console.log(resTab[0][1]);
 }
+
+// Calulates PCC for (Log(X), Y) && (X * Log(X), Y) && (X^2 * Log(X), Y) && (X^3 * Log(X), Y)
+for (let i = 0; i <= 3; i++) {
+  const tabX = varX.map((val) => Math.pow(val, i) * Math.log(val));
+  let nPow = "n";
+  if (i > 1) nPow += `^${i}`;
+  resTab.push([PearsonCoeff(tabX, varY), `O(${i ? `${nPow} ` : ""}log n)`]);
+}
+
+// Calulates PCC for (2^X, Y)
+const tabX = varX.map((val) => Math.pow(2, val));
+resTab.push([PearsonCoeff(tabX, varY), "O(2^n)"]);
+
+// sort to find the best Correlation
+resTab.sort((a, b) => b[0] - a[0]);
+
+return console.log(resTab[0][1]);
